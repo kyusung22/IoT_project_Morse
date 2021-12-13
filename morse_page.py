3f72516b943605bf8bfea390069c59ea5df20e49
윤규성 <rbtjd0312@naver.com>
2021-12-13 19:38:34 +0900

    ver-final

--- morse_page.py
+++ morse_page.py
@@ -12,7 +12,7 @@ def home():
 button1_pin = 15
 button2_pin = 14 
 count1= 0
-count2=0
+count2= 0
 red = 17
 green= 27 
 blue = 22
@@ -49,6 +49,7 @@ output_eng=''
 final_word=''
 once = True
 
+#모스 --> 알파벳 딕셔너리
 morse_eng = {
     '.-'   : 'A',
 	'-...' : 'B',
@@ -78,7 +79,7 @@ morse_eng = {
 	'--..' : 'Z',
     '.....' : ' ' 
 }
-
+#알파벳 --> 모스 딕셔너리
 eng_morse = {
     'A'   : '.-'   ,
 	'B' : '-...',
@@ -108,38 +109,34 @@ eng_morse = {
 	'Z' : '--..',
     	' ' : '.....' 
 }
+
+#모스부호를 입력받는 경우 사용하는 함수
 @app.route("/merge_") 
 def merge_(s):
     global word_morse
     word_morse = word_morse + s
-
+#알파벳을 입력받는 경우 사용하는 함수
 @app.route("/merge_e") 
 def merge_e(s1):
     global word_eng
     word_eng = word_eng+ s1
 
+#알파벳--> 모스부호 에서 출력되는 영어 문자열
 @app.route("/merge_o") 
 def merge_o(s1):
     global output_eng
     output_eng = output_eng+ s1
 
-@app.route("/merge_e_c") 
-def merge_e_c(s1):
-    global word_eng_c
-    word_eng_c=word_eng_c+s1
-   #  print(word_eng_c)
-
-@app.route("/merge_e_c_toNULL") 
-def merge_e_c_toNULL():
-    global word_eng_c
-    word_eng_c=''
-
+#입력받는 모스부호를 구분자'/'를 기준으로 나누는 함수
 @app.route("/make_word") 
 def make_word(word_m):
     global word_list
     word_list = word_m.split('/')
-    #print("word list :  " + str(word_list))
 
+strErrorState= "There is not incorrect Morsecode"
+strError= False
+
+#입력받는 모스부호를 morse_eng 딕셔너리에서 매칭시키고 merge하는 함수
 @app.route("/word_view") 
 def word_view():
     global final_word
@@ -147,42 +144,26 @@ def word_view():
     global word_list
     global morse_eng
     global word_eng
+    global strError
 
     make_word(word_morse)
 
     try:
         for i in range(len(word_list)-1):
             merge_e(morse_eng[word_list[i]])
-           # print("for loop inner : "+morse_eng[word_list[i]])
     except KeyError:
-        print("Invalid Morsecode included.1111111111111")
+        strError= True
 
-    print("wordng= "+ word_eng)
+#   print("wordng= "+ word_eng)
     final_word=word_eng
 
-@app.route("/word_view_while") 
-def word_view_while():
-    global word_eng_c
-    global word_list
-    global word_morse
-
-    make_word(word_morse)
-
-    try:
-        for i in range(len(word_list)):
-            merge_e_c(morse_eng[word_list[i]])
-    except KeyError:
-        pass
-    
-    merge_e_c_toNULL()
-
+#모스 --> 알파벳 번역 후 LED 점멸과 Buzzer 소리를 실행시키는 함수 
 @app.route("/play") 
 def play():
     global output_eng
     global p
     global speed
     
-
     for i in range(len(output_eng)-1):
         print(output_eng[i])
         sleep(0.3)
@@ -210,39 +191,36 @@ def play():
 
     return str("ok")
 
-
+# 단신호 입력 시 RGB LED에서 BLUE 점등
 @app.route("/blueLight")
 def blueLight():
     GPIO.output(blue,1)
     sleep(0.2)
     GPIO.output(blue,0)
 
+# 장신호 입력 시 RGB LED에서 GREEN 점등
 @app.route("/greenLight")
 def greenLight():
     GPIO.output(green,1)
     sleep(0.2)
     GPIO.output(green,0)
 
+# 구분자 신호 입력 시 RGB LED에서 RED 점등
 @app.route("/redLight")
 def redLight():
     GPIO.output(red,1)
     sleep(0.2)
     GPIO.output(red,0)
 
-@app.route("/yelLight")
-def yelLight():
-    GPIO.output(red,1)
-    GPIO.output(green,1)
-    sleep(0.2)
-    GPIO.output(red,0)
-    GPIO.output(green,0)
-
+# push button을 통해 모스부호를 입력받는 함수
 try:
     @app.route("/inputMode")
     def inputMode():
         global count1
         global count2
         global word_morse
+        count1= 0
+        count2= 0
         word_morse= ''
         isExit = 0
 
@@ -250,31 +228,26 @@ try:
             if GPIO.input(button1_pin) == GPIO.HIGH:
                 sleep(0.2)
                 count1+=1
-                sleep(0.1)
+                sleep(0.2)
             if GPIO.input(button1_pin) == GPIO.LOW:
                 if(count1==1):       
                     blueLight()
                     merge_(".")
                     print("" + word_morse)
-                    
-
                 elif (count1>=2):
                     greenLight()
                     merge_("-")
                     print("" + word_morse)
-
                 count1=0
 
             if GPIO.input(button2_pin) == GPIO.HIGH:
                 sleep(0.2)
                 count2+=1
-
+                sleep(0.1)
             if GPIO.input(button2_pin) == GPIO.LOW:
                 if(count2>=1):
                     merge_("/")
-                    yelLight() 
-                    word_view_while()
-                    # 버튼2 푸쉬 시 마다 글자가 나오게 하려면 ? --> 글자break가 한번에가 아닌 그때마다 완성되어야 
+                    redLight() 
                 count2=0
                 
             if ('//' in word_morse):
@@ -285,13 +258,12 @@ try:
 
             sleep(0.1)    # 0.3초 딜레이 
         word_view()
-    
 except :
     pass
 
+#모스부호를 입력받은 후 딕셔너리에서 매칭시켜 완성한 알파벳을 다시 웹으로 전송하는 코드
 @app.route('/sub_Mor', methods=['POST','GET'])                       # index.html에서 이 주소를 접속하여 해당 함수를 실행
 def sub_Mor():
-    print("sub_mor")
     if request.method == 'POST':
         print("post conversion")
         pass
@@ -299,15 +271,20 @@ def sub_Mor():
     elif request.method=='GET':
         global word_morse
         global final_word
+        global strErrorState
         word_view()
-        print("return = "+word_morse)
-        print("word-english ? : "+final_word)
+
         word_morse2= word_morse
         final_word2=final_word
         word_morse=''
         final_word=''
-        return render_template('morse.html', nata=str(word_morse2), mata= str(final_word2))
 
+        if(strError == False):
+            return render_template('morse.html', nata=str(word_morse2), mata= str(final_word2))
+        elif(strError):
+            return render_template('morse.html', nata = str(word_morse2), mata= str(strErrorState))
+
+#알파벳을 입력받은 후 딕셔너리에서 매칭시켜 완성한 모스부호를 다시 웹으로 전송하는 코드
 @app.route('/sub_Eng', methods=['POST','GET'])                       # index.html에서 이 주소를 접속하여 해당 함수를 실행
 def sub_Eng():
     if request.method=='GET':
@@ -327,9 +304,11 @@ def sub_Eng():
         except KeyError:
             output_eng=" Sorry ! Only support for Alphabet. "
         
-        print("@@ : "+ input_eng)
-        print("##################" + output_eng)
+        print("입력받은 알파벳 : "+ input_eng)
+        print("매칭된 모스부호 : " + output_eng)
+
     return render_template('morse.html', data=str(output_eng))
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
